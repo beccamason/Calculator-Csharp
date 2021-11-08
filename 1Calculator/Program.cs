28f1c13f05bc0ccb03725398e63a25747cbed3d4
beccamason <beccamason28@gmail.com>
2021-11-08 14:27:56 +0000

    Added simple Addition & Multiplier

--- 1Calculator/Program.cs
+++ 1Calculator/Program.cs
@@ -6,7 +6,25 @@ namespace _1Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+/*            //Exercist 2
+            Console.Write("Hey! What's your name? ");
+            string aFriend = Console.ReadLine();
+            Console.WriteLine("Welcome to the Calculator " + aFriend);*/
+            
+            //Exercise 3
+/*            Console.Write("Hey! What's the radius of your circle? ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double area = (radius * radius * (Math.PI));
+            Console.WriteLine("Great, the area of your circle is " + area);*/
+
+            Console.Write("Hey! Pick a number: ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Thanks! Now please pick another number: ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+            double sum = firstNumber + secondNumber;
+            double product = firstNumber * secondNumber;
+            Console.WriteLine("Great, the sum of those two number is " + sum + " and the product of your numbers is " + product);
+
         }
     }
 }
